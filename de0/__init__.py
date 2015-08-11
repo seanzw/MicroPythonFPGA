faec7ae0bb695e641dbe7bb81831097160bf9f61
Zhengrong Wang <seanyukigeek@gmail.com>
2015-08-11 19:08:37 +0100

    basic led

--- de0/__init__.py
+++ de0/__init__.py
@@ -1,1 +1,2 @@
 from de0.de0mem_py import *
+from de0.de0led import *
--- de0/de0led.py
+++ de0/de0led.py
@@ -1,4 +1,33 @@
 # Led python module based on de0mem
 
-import de0mem as mm
+LED_PIO_BASE = 0x10040 + 0xFF200000;
+
+class led(object):
+	"""led class"""
+	def __init__(self, idx):
+		super(led, self).__init__()
+		if idx > 7:
+			self.idx = 7
+		elif idx < 0:
+			self.idx = 0
+		else:
+			self.idx = idx
+
+	def on(self):
+		mask = read_uint32_from_pa(LED_PIO_BASE) | (1 << self.idx)
+		write_uint32_to_pa(LED_PIO_BASE, mask)
+
+	def off(self):
+		mask = read_uint32_from_pa(LED_PIO_BASE) & (~(1 << self.idx))
+		write_uint32_to_pa(LED_PIO_BASE, mask)
+
+	def status(self):
+		return bool(read_uint32_from_pa(LED_PIO_BASE) & (1 << self.idx))
+
+	def toggle(self):
+		if self.status():
+			self.off()
+		else:
+			self.on()
+
 
