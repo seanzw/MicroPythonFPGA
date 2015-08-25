a72f29f23d07f3884477930aa7091138d1f5263a
Johnson Tan <phisiart@gmail.com>
2015-08-25 15:40:52 +0100

    move led and sgdma

--- de0/__init__.py
+++ de0/__init__.py
@@ -1,4 +1,3 @@
 from de0.de0mem_py import *
-from de0.de0led import LED
 from de0.de0hps_led import HPS_LED
 from de0.de0gsensor import GSensor
--- led/de0led.py
+++ led/de0led.py
@@ -1,15 +1,14 @@
 # Led python module based on de0mem
 from de0.de0mem_py import *
-import de0.hps_0 as hps_0
+import led.hps_0 as hps_0
 
 class LED(object):
 	
 	def __init__(self, idx):
-		super(led, self).__init__()
 		
 		if 'LED_PIO' not in dir(hps_0):
 			raise RuntimeError('Cannot find "LED_PIO" in hps_0')
-		__base__ = 0xFF200000 + hps_0.LED_PIO.BASE
+		LED.__base__ = 0xFF200000 + hps_0.LED_PIO.BASE
 		
 		if idx > 7:
 			self.idx = 7
@@ -19,15 +18,15 @@ class LED(object):
 			self.idx = idx
 
 	def on(self):
-		mask = read_uint32_from_pa(led.__base__) | (1 << self.idx)
-		write_uint32_to_pa(LED_PIO_BASE, mask)
+		mask = read_uint32_from_pa(LED.__base__) | (1 << self.idx)
+		write_uint32_to_pa(LED.__base__, mask)
 
 	def off(self):
-		mask = read_uint32_from_pa(led.__base__) & (~(1 << self.idx))
-		write_uint32_to_pa(LED_PIO_BASE, mask)
+		mask = read_uint32_from_pa(LED.__base__) & (~(1 << self.idx))
+		write_uint32_to_pa(LED.__base__, mask)
 
 	def status(self):
-		return bool(read_uint32_from_pa(led.__base__) & (1 << self.idx))
+		return bool(read_uint32_from_pa(LED.__base__) & (1 << self.idx))
 
 	def toggle(self):
 		if self.status():
